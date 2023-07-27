61ba1c6f50c933b8d171810f52ae7b8eadccb29d
Julian0rlowski <julian.sz.orlowski@gmail.com>
2023-07-27 10:35:10 +0200

    USA flag no update I will leave it messy as it is and focus on something else

--- USA/index.js
+++ USA/index.js
@@ -1,5 +1,5 @@
 const starContainer = document.getElementById("star-container");
-const numberOfRows = 13;
+const numberOfRows = 9;
 const starsPerRow = [6, 5, 6, 5, 6, 5, 6, 5, 6];
 
 function createStars(rows, starsPerRow) {
@@ -20,3 +20,16 @@ function createStars(rows, starsPerRow) {
 createStars(numberOfRows, starsPerRow);
 
 
+const rootVars = getComputedStyle(document.documentElement);
+const unit = parseInt(rootVars.getPropertyValue('--unit'));
+const starWidth = unit * 0.316;
+const container6Width = unit * 7.6;
+const container5Width = (container6Width - starWidth * 6) / 5 * 4 + starWidth * 5;
+
+const starRow5 = document.getElementsByClassName('star-row-5');
+
+// for (let i = 0; i < starRow5.length; i++) {
+//   starRow5[i].style.width = `${container5Width}px`;
+// }
+
+console.log(starRow5, container5Width, container6Width)
