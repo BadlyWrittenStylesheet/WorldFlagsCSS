e6eafcb581494bf3a3fac7263b2af87fe8ebab4a
Julian0rlowski <julian.sz.orlowski@gmail.com>
2023-08-04 10:55:46 +0200

    St lucia done. I think.

--- St Lucia not done/index.js
+++
@@ -1,6 +0,0 @@
-// const whiteTriangle = document.getElementById("white");
-// const unit = getComputedStyle(root).getPropertyValue("--unit");
-
-// const whiteBottomBorder = parseFloat(unit) * Math.sqrt(55);
-
-// whiteTriangle.style.setProperty("--border-bottom", `${whiteBottomBorder}px solid`);
